f58e679bf878cb0eaa33cb2f58ca843df4d1146b
vanebrina <seider.sanin22135@ucaldas.edu.co>
2024-03-14 17:30:27 -0500

    Create model user

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // Importacin del franwork expres para realizar solicitud http
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 //poder accerder a leer eel archivo .env
 require("dotenv").config();
 const PORT_URL = process.env.PORT || 3005;
 
-
 app.listen(PORT_URL, () => {
     console.log(`server runing on port ${PORT_URL}`);
 });
+
+const mongo_connect = () => {
+    try {
+        mongoose
+            .connect(process.env.DATABASE_CONNECTION_STRING)
+            .then(() => {
+                console.log("Connected to MongoDB");
+            })
+            .catch ((err) => {
+                console.log("lbs");
+            });
+    } catch (err) {
+        console.log(err);
+    };
+};
+
+mongo_connect();
---
+++ src/models/user_model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+    document: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    user_email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    user_active: {
+        type: String,
+        required: true,
+        default: false
+    }
+});
+
+
+module.exports = mongoose.model("User", userSchema);
